6c03fc1990519564673d43b320db5c9094b7f864
Stanislaww <stMdQT@bk.ru>
2026-05-21 13:39:31 +0300

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -16,7 +16,7 @@ $(() => {
         .attr({
           id: "theme-css",
           rel: "stylesheet",
-          href: `${pathCss}/${file}?t=${Date.now()}`,
+          href: `css/${file}?t=${Date.now()}`,
         })
         .appendTo("head");
     }
